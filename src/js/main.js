97ea3f6207766d9544309e3d088752a1027bfc5f
Andrey Strelcov <astrelcov@yahoo.com>
2024-12-28 00:57:05 +0200

    edit menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
+console.log("Файл script.js успешно подключён!");
 
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.header-menu'),
+  menuItem = document.querySelectorAll('header-menu__item'),
+  hamburger = document.querySelector('.menu-mob');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('menu-mob__active');
+    menu.classList.toggle('header-menu__active');
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('menu-mob__active');
+      menu.classList.toggle('header-menu__active');
+    })
+  })
+})
